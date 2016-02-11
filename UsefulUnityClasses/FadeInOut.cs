bffd2a2b0f1e53883a193ebca90c17a71aedfef7
matt123miller <matt123miller@hotmail.co.uk>
2016-02-11 21:03:30 +0000

    A useful collection of classes and packages for Unity.

---
+++ UsefulUnityClasses/FadeInOut.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public  class FadeInOut : MonoBehaviour
+{
+
+    public static Image image;
+    public float fadeOverTime;
+    
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.enabled = true;
+    }
+    
+    void Start() {
+        
+        StartCoroutine(FadeToClear(fadeOverTime));
+    }
+    
+
+    public static IEnumerator FadeToClear(float fadeOverTime)
+    {
+        for (float f = fadeOverTime; f >= 0; f -= 0.01f) {
+            
+            Color c = image.color;
+            c.a = f;
+            image.color = c;
+            yield return null;
+        }
+    }   
+    
+    public static IEnumerator FadeToBlack(float fadeOverTime)
+    {
+        for (float f = 0f; f <= fadeOverTime; f += 0.01f) {
+            
+            Color c = image.color;
+            c.a = f;
+            image.color = c;
+            yield return null;
+        }
+    }   
+}
